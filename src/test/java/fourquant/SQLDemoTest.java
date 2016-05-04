ecf062a3e052dfd522d8a9c8e6d41083cd1e8731
kmader <kevinmader@4quant.com>
2016-05-04 14:03:26 +0200

    modifying tests to connect to pacs

--- src/test/java/fourquant/SQLDemoTest.java
+++ src/test/java/fourquant/SQLDemoTest.java
@@ -17,24 +17,20 @@ import java.io.Serializable;
 public class SQLDemoTest implements Serializable {
     final String fijiPath = "/Applications/Fiji.app/Contents/";
     final ImageJSettings ijs = new USBImageJSettings(fijiPath,false,false,false);
-    final int bindPort = 8080;
-    final String bindAddress = "0.0.0.0";
+    final int bindPort = 11112;
+    final String bindAddress = "localhost";
+    // we don't want a directory based table at all
+    final Option<String> basePathInput = Option.empty();
+
+    final SQLContext sq = fourquant.db.demo.createSQLTool(basePathInput,bindPort,bindAddress,"TestTable",false,true,
+            ijs);
 
     //@Test
-    public void testSetupSparkContext() {
+    public void testCreateTemporaryPacs() {
 
-        // show the spark context runs
-        final SparkContext sc = new SparkContext("local[2]","SQL Demo Test");
-        System.out.println(sc.appName());
-
-    }
-
-    @Test
-    public void testCreateSQLTool() {
-        final Option<String> basePathInput = Option.empty();
-
-        SQLContext sq = fourquant.db.demo.createSQLTool(basePathInput,bindPort,bindAddress,"TestTable",false,true,
-                ijs);
+        sq.sql("CREATE TEMPORARY TABLE DemoPacs\n" +
+                "USING fourquant.pacs\n" +
+                "OPTIONS (debug \"true\", debug_name \"Anonymous Sid 10007\")");
 
         // execute the command SHOW TABLES
         DataFrame allTables = sq.sql("SHOW TABLES");
@@ -46,4 +42,59 @@ public class SQLDemoTest implements Serializable {
             }
         });
     }
+
+    @Test
+    public void testCOPDPlugin() {
+
+        sq.sql("CREATE TEMPORARY TABLE DownloadPacs\n" +
+                "USING fourquant.pacs\n" +
+                "OPTIONS (debug \"true\", debug_name \"Anonymous Sid 10007\")");
+
+
+        // execute the command SHOW TABLES
+        DataFrame tabDesc = sq.sql("DESCRIBE DownloadPacs");
+        // show all of the results
+        tabDesc.toJavaRDD().foreach(new VoidFunction<Row>() {
+            @Override
+            public void call(Row row) throws Exception {
+                System.out.println(row.mkString(","));
+            }
+        });
+
+        String simpleQuery = "SELECT seriesInstanceUID,fetch_dicom_imagej(studyInstanceUID,seriesInstanceUID) " +
+                "FROM DownloadPacs WHERE success=true LIMIT 1";
+
+        DataFrame simpleImages = sq.sql(simpleQuery);
+        // show all of the results
+        simpleImages.toJavaRDD().foreach(new VoidFunction<Row>() {
+            @Override
+            public void call(Row row) throws Exception {
+                System.out.println(row.mkString(","));
+            }
+        });
+
+
+        String fImage = "fetch_dicom_imagej(studyInstanceUID,seriesInstanceUID)";
+
+
+        String segCommand = "run2("+fImage+",'"+USBImageJSettings.SegmentLung()+"','')";
+
+        String copdQuery = "SELECT seriesInstanceUID,runrow("+segCommand+",'"+
+                USBImageJSettings.StageLung()+"','') FROM " +
+                "DownloadPacs WHERE success=true LIMIT 1";
+
+        System.out.println("copdQuery:"+copdQuery);
+        DataFrame allTables = sq.sql(copdQuery);
+        // show all of the results
+        allTables.toJavaRDD().foreach(new VoidFunction<Row>() {
+            @Override
+            public void call(Row row) throws Exception {
+                System.out.println(row.mkString(","));
+            }
+        });
+    }
+
+
+
+
 }
